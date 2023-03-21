0a18b7e5a21840610fa035c696412187b185c3a3
Volodymyr <fedunivvolodymyr@gmail.com>
2023-03-21 18:40:20 +0200

    Homework 2

--- lesson_02/script.js
+++ lesson_02/script.js
@@ -12,24 +12,24 @@
 // 2.
 // Є такий код:
 
-let x = 1;
-let y = 2;
-
-let res1 = '1' + '2'  // Допишіть код, необхідно використовувати змінні x і y
-    console.log(res1); // ""12""
-console.log(typeof res1); // ""string""
-
-let res2 = '2' + true // Допишіть код, необхідно використовувати змінні x і y
-    console.log(res2); // ""true2""
-console.log(typeof res2); // ""string""
-
-let res3 = Boolean(y-x) // Допишіть код, необхідно використовувати змінні x і y
-    console.log(res3); // true
-console.log(typeof res3); // ""boolean""
-
-let res4 = true / false // Допишіть код, необхідно використовувати змінні x і y
-    console.log(res4); // NaN
-console.log(typeof res4); // ""number""
+// let x = 1;
+// let y = 2;
+//
+// let res1 = '1' + '2'  // Допишіть код, необхідно використовувати змінні x і y
+//     console.log(res1); // ""12""
+// console.log(typeof res1); // ""string""
+//
+// let res2 = '2' + true // Допишіть код, необхідно використовувати змінні x і y
+//     console.log(res2); // ""true2""
+// console.log(typeof res2); // ""string""
+//
+// let res3 = Boolean(y-x) // Допишіть код, необхідно використовувати змінні x і y
+//     console.log(res3); // true
+// console.log(typeof res3); // ""boolean""
+//
+// let res4 = true / false // Допишіть код, необхідно використовувати змінні x і y
+//     console.log(res4); // NaN
+// console.log(typeof res4); // ""number""
 
 
 // Допишіть код, щоб в консолі браузера з'явилися рядки, які написані в коментарях, оперуючи при цьому змінними х і у.

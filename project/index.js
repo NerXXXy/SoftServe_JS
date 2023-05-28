b178f273be530409cbaedaf7ea8c32c610b634c5
Volodymyr <fedunivvolodymyr@gmail.com>
2023-05-28 05:43:59 +0300

    .

--- project/index.js
+++ project/index.js
@@ -1,13 +1,13 @@
 let points = 0;
 
-let clickPower = 0.1;
+let clickPower = 1;
 let clickPowerLVL = 1;
-let nextPowerUpgradeCost = 10 * clickPower +(clickPowerLVL * 1.2);
+let nextPowerUpgradeCost = 10 * clickPower + (clickPowerLVL * 3);
 
-// let autoClickCD = 10000;
-// let autoClickPower = 0;
-// let autoClickCDUpgrade = 20;
-// let autoClickPowerUpgrade = 0;
+ let autoClickCD = 1000;
+ let autoClickPower = 1;
+ let autoClickCDUpgrade = 20;
+ let autoClickPowerUpgradeCost = 0;
 
 const display = document.querySelector('#display');
 const button = document.querySelector('#button');
@@ -26,14 +26,14 @@ button.onclick = play;
 clickPowerButton.onclick = upgradePower;
 
 function play() {
-    button.onclick = () => counter.textContent =`Your points: ${points = Math.trunc( (points + clickPower) * 50 ) / 50}`;
+    button.onclick = () => counter.textContent =`Your points: ${points = points + clickPower}`;
 }
 function upgradePower(){
     if(points - nextPowerUpgradeCost >= 0){
         points = points - nextPowerUpgradeCost
-        counter.textContent =`Your points: ${Math.trunc( points * 100 ) / 100}`;
-        clickPower = clickPower + Math.trunc( (clickPower * 1.3) * 100 ) / 100;
-        nextPowerUpgradeCost = nextPowerUpgradeCost * 2;
+        counter.textContent =`Your points: ${points}`;
+        clickPower = clickPower + 1;
+        nextPowerUpgradeCost = nextPowerUpgradeCost * 3 + clickPower;
         clickPowerLVL = clickPowerLVL + 1;
         powerStatusText.textContent = `Your click power is ${clickPower}. ${nextPowerUpgradeCost} points for click power ${clickPowerLVL + 1} LVL`
     }
@@ -45,3 +45,13 @@ function upgradePower(){
 // function autoClick(){
 //
 // }
+
+function autoClick() {
+    setInterval(() => {
+        points = points + autoClickPower;
+        counter.textContent = `Your points: ${points}`;
+    }, autoClickCD);
+}
+
+// Виклик функції для автоматичного отримання points
+autoClick();
